cb60a4cee81f1eec18021ff0292d783bd7802031
grdall <alex.thogar@gmail.com>
2020-05-18 19:37:48 +0200

    node + heroku server.js, updated packages

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
+// app.use(favicon(__dirname + 'favicon.ico'));
+
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+
+app.listen(port);
