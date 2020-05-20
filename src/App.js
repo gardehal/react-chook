78f356553f939bf6cb502a9a5f418a7a1d95c811
grdall <alex.thogar@gmail.com>
2020-05-20 16:37:24 +0200

    heroku env test(works locally with .env)

--- src/App.js
+++ src/App.js
@@ -26,18 +26,7 @@ class App extends React.Component
     {
         super(props);
 
-        // let env = import("./secrets/env");
-        let env =
-        {
-            apiKey: process.env.fb_apikey,
-            authDomain: process.env.fb_authdom,
-            databaseURL: process.env.fb_dburl,
-            projectId: process.env.fb_pid,
-            storageBucket: process.env.fb_storebuck,
-            messagingSenderId: process.env.fb_msgid
-        }
-
-        firebase.initializeApp(`${process.env.FirebaseObject}`);
+        firebase.initializeApp(process.env.FB_OBJECT);
 
         // getApiExample("https://rallycoding.herokuapp.com/api/music_albums");
 
