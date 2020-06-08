dab5d06d99bd9935658a9c2394d19eb46ff9aed1
grdall <alex.thogar@gmail.com>
2020-06-08 19:42:52 +0200

    Update RecipeDetailsPage.jsx

--- src/components/RecipeDetailsPage.jsx
+++ src/components/RecipeDetailsPage.jsx
@@ -12,6 +12,12 @@ import { DB_RECIPE, DB_FETCH_FAILED, NORWEGIAN_KRONER, MINUTES, PREPARATION, TOT
 
 // Component imports
 import { RecipeCard } from "./common/RecipeCard";
+import { Preparation } from "../models/enums/Preparation";
+import { QuantityUnit } from "../models/enums/QuantityUnit";
+import { CookingMethod } from "../models/enums/CookingMethod";
+import { TempratureUnit } from "../models/enums/TempratureUnit";
+import { RecipeType } from "../models/enums/RecipeType";
+import { Difficulty } from "../models/enums/Difficulty";
 
 class RecipeDetailsPage extends React.Component
 {
@@ -25,7 +31,7 @@ class RecipeDetailsPage extends React.Component
     {
         let fromUrl = window.location.search.split("?" + DB_RECIPE + "=");
         let id = fromUrl[1];
-        getRecipeData("recipe_id", id, 1);
+        getRecipeData("id", id, 1);
 
         getRecipeData();
     }
@@ -53,61 +59,85 @@ class RecipeDetailsPage extends React.Component
         if(!recipe || this.props.recipeLoading)
             return renderLoading(true, this.props.contrastmode);
 
-        if(this.props.recipeResult.length < 1 || this.props.recipeError)
+        if(this.props.recipeResult.length !== 1 || this.props.recipeError)
             return renderError(DB_FETCH_FAILED, true, this.props.contrastmode);
 
-        setTitle(recipe.recipe_title);
+        // Recipe
+        let id = recipe.id;
+        let title = recipe.title;
+        let cost = recipe.cost;
+        let totalCost = recipe.total_cost;
+        let type = recipe.type;
+        let difficulty = recipe.difficulty;
+        let rating = recipe.rating;
+        let portions = recipe.portions;
+        let time_preparation = recipe.time_preparation;
+        let time_total = recipe.time_total;
+        let cooking_method = recipe.cooking_method;
+        let cooking_method_temperature = recipe.cooking_method_temperature;
+        let cooking_method_temperature_unit = recipe.cooking_method_temperature_unit;
+        let main_protein = recipe.main_protein;
+        let ingredients = recipe.recipe_ingredients;
+        let sub_recipes = recipe.sub_recipes;
+        let instructions = recipe.instructions;
+        let notes = recipe.notes;
+
+        setTitle(title);
 
         // Extract data that can be undefined to local vars and display based on undefined or not
-        let method = (recipe.recipe_cooking_method ? recipe.recipe_cooking_method : "");
-        let methodTemp = (recipe.recipe_cooking_method_temperature ? recipe.recipe_cooking_method_temperature : "");
-        let methodTempUnit = (recipe.recipe_cooking_method_temperature_unit ? recipe.recipe_cooking_method_temperature_unit : "")
+        let method = CookingMethod[cooking_method] ?? "";
+        let methodTemp = cooking_method_temperature ?? "";
+        let methodTempUnit = TempratureUnit[cooking_method_temperature_unit] ?? "";
 
         // Make a JSX array of all the ingredients and render this array later
         let ingredientsJsx = [];
-        for(let ingredientsIndex = 0; ingredientsIndex < recipe.recipe_ingredients.length; ingredientsIndex++)
+        for(let i = 0; i < ingredients.length; i++)
             ingredientsJsx.push(
-                <p key={"ingredient" + ingredientsIndex} style={{ ...getTextColor(this.props.contrastmode) }}>
-                    {recipe.recipe_ingredients[ingredientsIndex].ingredient_name}
+                <p key={"ingredient" + i} style={{ ...getTextColor(this.props.contrastmode) }}>
+                    { " - "
+                        + (ingredients[i].quantity ? ingredients[i].quantity.toString() + " " : "")
+                        + (ingredients[i].quantity_unit ? QuantityUnit[ingredients[i].quantity_unit] + " " : "")
+                        + ingredients[i].ingredient.name 
+                        + (ingredients[i].preparation ? " " + Preparation[ingredients[i].preparation] : "") }
                 </p>);
                 
         // Make a JSX array of all the sub recipes (if any) and render this array later
         let subRecipeJsx = [];
-        if(recipe.recipe_sub_recipes)
-            for(let subRecipeIndex = 0; subRecipeIndex < recipe.recipe_sub_recipes.length; subRecipeIndex++)
-                subRecipeJsx.push(<RecipeCard key={"subRecipe" + subRecipeIndex} recipe={recipe.recipe_sub_recipes[subRecipeIndex]} history={this.props}
+        if(sub_recipes)
+            for(let i = 0; i < sub_recipes.length; i++)
+                subRecipeJsx.push(<RecipeCard key={"subRecipe" + i} recipe={sub_recipes[i]} history={this.props}
                     subRecipe={true} contrastmode={this.props.contrastmode}/>);
 
         // Make a JSX array of all the instructions and render this array later
         let instructionsJsx = [];
-        for(let instructionsIndex = 0; instructionsIndex < recipe.recipe_instructions.length; instructionsIndex++)
+        for(let i = 0; i < instructions.length; i++)
             instructionsJsx.push(
-                <p key={"instruction" + instructionsIndex} style={{ ...getTextColor(this.props.contrastmode) }}>
-                    {recipe.recipe_instructions[instructionsIndex]}
+                <p key={"instruction" + i} style={{ ...getTextColor(this.props.contrastmode) }}>
+                    {instructions[i]}
                 </p>);
 
         // Make a JSX array of all the notes (if any) and render this array later
         let recipeNotesJsx = [];
-        if(recipe.recipe_notes)
-            for(let instructionsIndex = 0; instructionsIndex < recipe.recipe_notes.length; instructionsIndex++)
+        if(notes)
+            for(let i = 0; i < notes.length; i++)
                 recipeNotesJsx.push(
-                    <p key={"instruction" + instructionsIndex} style={{ ...getTextColor(this.props.contrastmode) }}>
-                        {recipe.recipe_notes[instructionsIndex]}
+                    <p key={"note" + i} style={{ ...getTextColor(this.props.contrastmode) }}>
+                        {notes[i]}
                     </p>);
 
         return (
             <div style={{ ...this.containerStyle, ...getLightBackgroundColor(this.props.contrastmode) }}>
-                <h3 style={{ ...this.titleStyle, ...getTextColor(this.props.contrastmode) }}>{recipe.recipe_title}</h3>
+                <h3 style={{ ...this.titleStyle, ...getTextColor(this.props.contrastmode) }}>{title}</h3>
 
                 <div style={{ ...this.infoContainerStyle }}>
-                    <p style={{ ...getTextColor(this.props.contrastmode) }}>{recipe.recipe_grade + " " + recipe.recipe_type + ", " + recipe.recipe_rating + "/10"}</p>
+                    <p style={{ ...getTextColor(this.props.contrastmode) }}>{Difficulty[difficulty] + " " + RecipeType[type] + ", " + rating + "/10"}</p>
                     
-                    <p style={{ ...getTextColor(this.props.contrastmode) }}>{recipe.recipe_portions + " " + PORTIONS
-                        + ", " + recipe.recipe_time_preparation + " " + MINUTES + " " + PREPARATION
-                        + ", " + recipe.recipe_time_total + " " + MINUTES + " " + TOTAL
-                        + ", " + recipe.recipe_price + " " + NORWEGIAN_KRONER}</p>
+                    <p style={{ ...getTextColor(this.props.contrastmode) }}>{portions + " " + PORTIONS
+                        + ", " + time_preparation + " " + MINUTES + " " + PREPARATION
+                        + ", " + time_total + " " + MINUTES + " " + TOTAL
+                        + ", " + cost + " " + NORWEGIAN_KRONER}</p>
                 </div>
-                {method ? <hr/> : (null)}
+                {method ? <hr/> : null}
 
                 <p style={{ ...getTextColor(this.props.contrastmode) }}>{method + " " + methodTemp + " " + methodTempUnit}</p>
                 <hr/>
@@ -117,7 +147,7 @@ class RecipeDetailsPage extends React.Component
                 <hr/>
 
                 {instructionsJsx}
-                {recipe.recipe_notes ? <hr/> : (null)}
+                {notes ? <hr/> : null}
 
                 {recipeNotesJsx}
             </div>);
