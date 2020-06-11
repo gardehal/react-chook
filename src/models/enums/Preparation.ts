ef0f05f35dff3cbf412741ea3a4356f8f34de5ba
grdall <alex.thogar@gmail.com>
2020-06-11 18:45:19 +0200

    enums 2/4

--- src/models/enums/Preparation.ts
+++ src/models/enums/Preparation.ts
@@ -1,3 +1,8 @@
+import { ENUM_NONE, ENUM_OTHER, ENUM_WHOLE, ENUM_CHOPPED, ENUM_MINCED, ENUM_QUBED, ENUM_QUARTERED,
+    ENUM_HALVED, ENUM_SLICED, ENUM_DRIED, ENUM_PITTED, ENUM_CORED, ENUM_GRATED, ENUM_MASHED,
+    ENUM_CRUSHED, ENUM_PRESSED
+} from "../../resources/language";
+
 export enum Preparation
 {
     NONE,
@@ -17,3 +22,21 @@ export enum Preparation
     CRUSHED,
     PRESSED,
 };
+
+export let PreparationDisplay: { [index: number]: string } = {};
+PreparationDisplay[Preparation.NONE] = ENUM_NONE;
+PreparationDisplay[Preparation.OTHER] = ENUM_OTHER;
+PreparationDisplay[Preparation.WHOLE] = ENUM_WHOLE;
+PreparationDisplay[Preparation.CHOPPED] = ENUM_CHOPPED;
+PreparationDisplay[Preparation.MINCED] = ENUM_MINCED;
+PreparationDisplay[Preparation.QUBED] = ENUM_QUBED;
+PreparationDisplay[Preparation.QUARTERED] = ENUM_QUARTERED;
+PreparationDisplay[Preparation.HALVED] = ENUM_HALVED;
+PreparationDisplay[Preparation.SLICED] = ENUM_SLICED;
+PreparationDisplay[Preparation.DRIED] = ENUM_DRIED;
+PreparationDisplay[Preparation.PITTED] = ENUM_PITTED;
+PreparationDisplay[Preparation.CORED] = ENUM_CORED;
+PreparationDisplay[Preparation.GRATED] = ENUM_GRATED;
+PreparationDisplay[Preparation.MASHED] = ENUM_MASHED;
+PreparationDisplay[Preparation.CRUSHED] = ENUM_CRUSHED;
+PreparationDisplay[Preparation.PRESSED] = ENUM_PRESSED;
--- src/models/enums/TempratureUnit.ts
+++ src/models/enums/TempratureUnit.ts
@@ -1,6 +1,14 @@
+import { ENUM_NONE, ENUM_CELSIUS, ENUM_FAHRENHEIT
+} from "../../resources/language";
+
 export enum TempratureUnit
 {
     NONE,
     C,
     F,
 };
+
+export let TempratureUnitDisplay: { [index: number]: string } = {};
+TempratureUnitDisplay[TempratureUnit.NONE] = ENUM_NONE;
+TempratureUnitDisplay[TempratureUnit.C] = ENUM_CELSIUS;
+TempratureUnitDisplay[TempratureUnit.F] = ENUM_FAHRENHEIT;
