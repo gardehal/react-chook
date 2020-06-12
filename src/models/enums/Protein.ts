7c20378f25e0eb2826ee82fedd7db796466f27f2
grdall <alex.thogar@gmail.com>
2020-06-12 05:12:31 +0200

    enum 3/4

--- src/models/enums/Protein.ts
+++ src/models/enums/Protein.ts
@@ -1,3 +1,7 @@
+import { ENUM_OTHER, ENUM_VEGAN, ENUM_VEGETARIAN, ENUM_BEEF, ENUM_LAMB, 
+    ENUM_PORK, ENUM_BIRD, ENUM_EGG, ENUM_FISH, ENUM_SEAFOOD, ENUM_DAIRY, 
+} from "../../resources/language";
+
 export enum Protein
 {
     OTHER,
@@ -16,5 +20,18 @@ export enum Protein
     DAIRY,
 };
 
-let ProteinDisplay: { [index: number]: string } = {};
-ProteinDisplay[Protein.OTHER] = "Annet"; // TODO From Language 
+export let ProteinDisplay: { [index: number]: string } = {};
+ProteinDisplay[Protein.OTHER] = ENUM_OTHER;
+ProteinDisplay[Protein.VEGAN] = ENUM_VEGAN;
+ProteinDisplay[Protein.VEGETARIAN] = ENUM_VEGETARIAN;
+ProteinDisplay[Protein.BEEF] = ENUM_BEEF;
+ProteinDisplay[Protein.LAMB] = ENUM_LAMB;
+ProteinDisplay[Protein.PORK] = ENUM_PORK;
+ProteinDisplay[Protein.SWINE] = ENUM_PORK;
+ProteinDisplay[Protein.POULTRY] = ENUM_BIRD;
+ProteinDisplay[Protein.FOWL] = ENUM_BIRD;
+ProteinDisplay[Protein.BIRD] = ENUM_BIRD;
+ProteinDisplay[Protein.EGG] = ENUM_EGG;
+ProteinDisplay[Protein.FISH] = ENUM_FISH;
+ProteinDisplay[Protein.SEAFOOD] = ENUM_SEAFOOD;
+ProteinDisplay[Protein.DAIRY] = ENUM_DAIRY;
--- src/models/enums/RecipeType.ts
+++ src/models/enums/RecipeType.ts
@@ -1,3 +1,6 @@
+import { ENUM_OTHER, ENUM_MAIN_DISH, ENUM_SIDE_DISH, ENUM_BREAKFAST, ENUM_APPETIZER, ENUM_DESSERT, ENUM_PASTRY, ENUM_SALAD, ENUM_DRINK, ENUM_SPICE 
+} from "../../resources/language";
+
 export enum RecipeType
 {
     OTHER,
@@ -5,9 +8,21 @@ export enum RecipeType
     SIDE_DISH,
     BREAKFAST,
     APPETIZER,
-    DESERT,
+    DESSERT,
     PASTRY,
     SALAD,
     DRINK,
     SPICE,
 };
+
+export let RecipeTypeDisplay: { [index: number]: string } = {};
+RecipeTypeDisplay[RecipeType.OTHER] = ENUM_OTHER;
+RecipeTypeDisplay[RecipeType.MAIN_DISH] = ENUM_MAIN_DISH;
+RecipeTypeDisplay[RecipeType.SIDE_DISH] = ENUM_SIDE_DISH;
+RecipeTypeDisplay[RecipeType.BREAKFAST] = ENUM_BREAKFAST;
+RecipeTypeDisplay[RecipeType.APPETIZER] = ENUM_APPETIZER;
+RecipeTypeDisplay[RecipeType.DESSERT] = ENUM_DESSERT;
+RecipeTypeDisplay[RecipeType.PASTRY] = ENUM_PASTRY;
+RecipeTypeDisplay[RecipeType.SALAD] = ENUM_SALAD;
+RecipeTypeDisplay[RecipeType.DRINK] = ENUM_DRINK;
+RecipeTypeDisplay[RecipeType.SPICE] = ENUM_SPICE;
